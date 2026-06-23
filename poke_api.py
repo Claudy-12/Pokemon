22d7bdce6cc1bc0bb76e07871ddae079c09c0710
Lara Maria Cigna <laracigna01@gmail.com>
2026-06-23 16:55:02 +0200

    aggiunge funzione scarica_pokemon

--- poke_api.py
+++ poke_api.py
@@ -1,11 +1,26 @@
 import requests
 
+URL_BASE = "https://pokeapi.co/api/v2/pokemon/"
+
 def scarica_pokemon(nome):
-    url = f"https://pokeapi.co/api/v2/pokemon/{nome.lower()}"
+    url = URL_BASE + nome.lower()
 
-    risposta = requests.get(url)
+    try:
+        risposta = requests.get(url, timeout=10)
 
-    if risposta.status_code == 200:
-        return risposta.json()
+        if risposta.status_code == 200:
+            return risposta.json()
+        else:
+            print("Errore: Pokémon non trovato")
+            return None
 
-    return None
+    except requests.exceptions.RequestException:
+        print("Errore di connessione")
+        return None
+
+
+if __name__ == "__main__":
+    pokemon = scarica_pokemon("pikachu")
+
+    if pokemon:
+        print("OK! Nome:", pokemon["name"])
